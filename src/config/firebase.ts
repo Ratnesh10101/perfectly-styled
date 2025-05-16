fb4c8d16d4363bed6a24d919a22212630034faa2
Vicky Rai <vickyks@hotmail.com>
2025-05-16 10:57:03 +0000

    installed firebase and init it to begin using SDK

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,14 +2,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDxycR2hu4dXoJnScRbRc3UZviWdEyVHYk",
+  authDomain: "perfectly-styled.firebaseapp.com",
+  projectId: "perfectly-styled",
+  storageBucket: "perfectly-styled.firebasestorage.app",
+  messagingSenderId: "377334244117",
+  appId: "1:377334244117:web:3fff766d728fd838a83f1c",
+  measurementId: "G-LLZ1XNNXET"
 };
 
 // Initialize Firebase
