22c2e90ac84071808f93450548e591de309f7075
Vicky Rai <vickyks@hotmail.com>
2025-05-27 12:58:50 +0000

    updated to fix typescript error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,7 +80,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
       },
-      typography: (theme) => ({
+      typography: (theme: (key: string) => string) => ({
         DEFAULT: {
           css: {
             h2: {
