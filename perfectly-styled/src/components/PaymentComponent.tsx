0765f7033e0961d0eede6a94119163be41b62ee4
Vicky Rai <vickyks@hotmail.com>
2025-05-27 17:36:32 +0000

    removed duplicate directory

--- perfectly-styled/src/components/PaymentComponent.tsx
+++
@@ -1,7 +0,0 @@
-
-// This component is no longer used and has been effectively removed.
-// Kept as an empty file to satisfy any existing imports gracefully during transition,
-// but can be fully deleted once all references are confirmed removed.
-export default function PaymentComponent() {
-  return null;
-}
